e2da4dd8a091361c74a3a9ca8886dce134e39655
sfrabby <fazlerabby6732@gmail.com>
2026-03-28 00:04:11 +0600

    LogOUT funtion complete

--- lib/screen/Home Page/ui.dart
+++ lib/screen/Home Page/ui.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../Auth/email_login/UI.dart';
 
 class firebase extends StatefulWidget {
   const firebase({super.key});
@@ -9,22 +12,26 @@ class firebase extends StatefulWidget {
 }
 
 class _firebaseState extends State<firebase> {
-
-  void LogOut()async{
+  void LogOut() async {
     FirebaseAuth.instance.signOut();
+    Get.offAll(() => LoginScreen());
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green  ,
+        actions: [
+          IconButton(
+            onPressed: () {
+              LogOut();
+            },
+            icon: Icon(Icons.exit_to_app),
+          ),
+        ],
+        backgroundColor: Colors.green,
         centerTitle: true,
-        title: Text(
-          "FireBase",
-          style: TextStyle(fontSize: 16),
-        ),
+        title: Text("FireBase", style: TextStyle(fontSize: 16)),
       ),
       body: Column(children: []),
     );
