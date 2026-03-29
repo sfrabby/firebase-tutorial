22943255972bff470e3f2b5e1beeefea8fd6fadb
sfrabby <fazlerabby6732@gmail.com>
2026-03-29 23:40:57 +0600

    UI error done

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: (FirebaseAuth.instance.currentUser != null)
-          ? firebase()
+          ? FirebaseHome()
           : LoginScreen(),
     );
   }
